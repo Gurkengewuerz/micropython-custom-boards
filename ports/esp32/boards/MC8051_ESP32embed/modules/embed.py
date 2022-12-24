bdd6ba8012b98043025d61e67d407ebb0fecc46f
Gurkengewuerz <niklas@schuetrumpf.net>
2022-12-24 23:03:08 +0100

    extend pin config to freezed modules for my custom board

--- ports/esp32/boards/MC8051_ESP32embed/modules/embed.py
+++ ports/esp32/boards/MC8051_ESP32embed/modules/embed.py
@@ -1,5 +1,5 @@
 from micropython import const
-from machine import Pin
+from machine import Pin, SDCard
 import neopixel
 
 # Pin Assignments
@@ -26,3 +26,28 @@ def rgb_led(r=0, g=0, b=0):
 # BUTTON
 BUTTON = const(0)
 button = Pin(BUTTON, Pin.IN, Pin.PULL_UP)
+
+RTC_ALERT = const(46)
+rtc_alert = Pin(RTC_ALERT, Pin.IN)
+
+MMC_DET = const(1)
+mmc_det = Pin(MMC_DET, Pin.IN)
+
+BATT_MON_RESET = const(48)
+batt_mon_reset = Pin(BATT_MON_RESET, Pin.OUT)
+
+LDO2 = const(5)
+ldo2 = Pin(LDO2, Pin.OUT)
+
+FET = const(4)
+fet = Pin(FET, Pin.OUT)
+
+mmc = SDCard(slot=1, width=1)
+
+MOD_AN = const(10)
+MOD_RST = const(45)
+MOD_CS = const(9)
+MOD_RX = const(6)
+MOD_TX = const(7)
+MOD_INT = const(16)
+MOD_PWM = const(39)
